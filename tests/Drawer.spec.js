b9c133893baf8ddb4a3f50a8444cd64cf21474a4
然则 <hualei.hl@alibaba-inc.com>
2017-07-04 11:30:10 +0800

    upgrade enzyme and rc-tools

---
+++ tests/Drawer.spec.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { render, mount } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
+import Drawer from '..';
+
+describe('Drawer', () => {
+  it('renders correctly', () => {
+    const wrapper = render(
+      <Drawer className="forTest" touch sidebar={<p>sidebar</p>}>
+        <p>React Sidebar is a sidebar component for React.</p>
+      </Drawer>
+    );
+    expect(renderToJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.find('.forTest').length === 1).toBe(true);
+  });
+
+  it('should close the sidebar', () => {
+    const cb = jest.fn();
+    const wrapper = mount(
+      <Drawer touch sidebar={<p>sidebar</p>} onOpenChange={cb} open>
+        <p>React Sidebar is a sidebar component for React.</p>
+      </Drawer>
+    );
+    wrapper.find('.rc-drawer-overlay').simulate('click');
+    expect(cb).toBeCalledWith(false, { overlayClicked: true });
+  });
+});
